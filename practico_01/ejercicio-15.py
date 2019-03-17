d0567a375c35c47accd0aaa6a67972eeae739cda
NadiConte <nadinaconte@gmail.com>
2019-03-17 16:30:36 -0300

    Ejercicio 15 - Apartado A

--- practico_01/ejercicio-15.py
+++ practico_01/ejercicio-15.py
@@ -3,21 +3,36 @@
 # B) Escriba ahora el programa de modo que almacene los números que el usuario introduzca en una lista y usa las
 # funciones max () y min () para calcular los números máximo y mínimo después de que el bucletermine.
 
+#Apartado A)
+def buscaValores():
+    max = min = 0
+    nro=input("Ingresar valor ('fin' para finalizar):")
+    while nro != 'fin':
+        if max == 0 and min == 0:
+            max = min = int(nro)
+        if int(nro) > max:
+            max = int(nro)
+        if int(nro) < min:
+            min = int(nro)
+        nro=input("Ingresar valor ('fin' para finalizar):")
+    print("El valor minimo es el numero: %s" %min)
+    print("El valor maximo es el numero: %s" %max)
 
+#Probar por consola
+buscaValores()
+
+
+#Apartado B)
 def busca_max_min():
     lista = []
     valores = input("Ingresar valor ('fin' para finalizar):")
     while valores.lower() != 'fin':
-        lista.append(valores)
+        lista.append(int(valores))
         valores = input("Ingresar valor ('fin' para finalizar):")
-    lista.sort()
-    min = lista[0]
-    lista.reverse()
-    max = lista[0]
-    print("El valor maximo es el numero:", max)
-    print("El valor minimo es el numero:", min)
+    print("El valor minimo es el numero: %s" %min(lista))
+    print("El valor maximo es el numero: %s" %max(lista))
     pass
-
 
 # Probar por consola.
 busca_max_min()
+
