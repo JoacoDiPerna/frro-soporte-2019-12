a8a4c50a33ad00ca2e2f08331aed28fb3fd60aef
JoacoDiPerna <joaquindiper@outlook.com>
2019-03-18 22:44:12 -0300

    Revert "Merge branch 'master' of https://github.com/JoacoDiPerna/frro-soporte-2019-12"
    This reverts commit 9f44deebc5b07cee2bed4b490c455c26d1656f47, reversing
    changes made to 363139046fc93f23862bc67b904c3cc8868cac6d.

--- practico_01/ejercicio-15.py
+++ practico_01/ejercicio-15.py
@@ -3,10 +3,10 @@
 # B) Escriba ahora el programa de modo que almacene los números que el usuario introduzca en una lista y usa las
 # funciones max () y min () para calcular los números máximo y mínimo después de que el bucletermine.
 
-# Apartado A)
-def busca_valores():
+#Apartado A)
+def buscaValores():
     max = min = 0
-    nro = input("Ingresar valor ('fin' para finalizar):")
+    nro=input("Ingresar valor ('fin' para finalizar):")
     while nro != 'fin':
         if max == 0 and min == 0:
             max = min = int(nro)
@@ -14,26 +14,25 @@ def busca_valores():
             max = int(nro)
         if int(nro) < min:
             min = int(nro)
-        nro = input("Ingresar valor ('fin' para finalizar):")
-    print("El valor minimo es el numero: %s" % min)
-    print("El valor maximo es el numero: %s" % max)
+        nro=input("Ingresar valor ('fin' para finalizar):")
+    print("El valor minimo es el numero: %s" %min)
+    print("El valor maximo es el numero: %s" %max)
+
+#Probar por consola.
+buscaValores()
 
 
-# Probar por consola.
-busca_valores()
-
-
-# Apartado B)
+#Apartado B)
 def busca_max_min():
     lista = []
     valores = input("Ingresar valor ('fin' para finalizar):")
     while valores.lower() != 'fin':
         lista.append(int(valores))
         valores = input("Ingresar valor ('fin' para finalizar):")
-    print("El valor minimo es el numero: %s" % min(lista))
-    print("El valor maximo es el numero: %s" % max(lista))
+    print("El valor minimo es el numero: %s" %min(lista))
+    print("El valor maximo es el numero: %s" %max(lista))
     pass
-
 
 # Probar por consola.
 busca_max_min()
+
