0ad79087dcf01109c918dbc1e51b8eb701516af1
JoacoDiPerna <joaquindiper@gmail.com>
2019-08-05 00:18:21 -0300

    Practico 07 - Primer Entrega
    Se finalizó con los requerimientos solicitados en el TP.
    Pendiente mejorar el código y validaciones.

--- practico_07/ABMSociosDesktop.py
+++ practico_07/ABMSociosDesktop.py
@@ -2,6 +2,7 @@ from practico_05.ejercicio_01 import Socio
 from practico_06.capa_negocio import NegocioSocio
 from tkinter import *
 from tkinter import ttk
+from enum import Enum
 
 # Creación de la ventana raíz.
 root = Tk()
@@ -22,6 +23,11 @@ root.geometry(
     )
 )
 root.protocol("WM_DELETE_WINDOW", lambda: close_window(root))
+
+class Opciones(Enum):
+    ALTA = 1
+    BAJA = 2
+    MODIFICACION = 3
 
 id_socio_seleccionado = IntVar()
 
@@ -61,15 +67,104 @@ def delete_frame_grid_controls():
         l.destroy()
 
 def create_buttons(cont):
-    Button(frame_buttons, text="Crear", width=10, anchor="center", padx=5, command="callback").grid(row=0,column=0)
-    Button(frame_buttons, text="Modificar", width=10, anchor="center", padx=5, command="callback").grid(row=0,column=1)
+    Button(frame_buttons, text="Crear", width=10, anchor="center", padx=5, command=lambda:add_or_update_socio(Opciones.ALTA)).grid(row=0,column=0)
+    Button(frame_buttons, text="Modificar", width=10, anchor="center", padx=5, command=lambda:add_or_update_socio(Opciones.MODIFICACION)).grid(row=0,column=1)
     Button(frame_buttons, text="Eliminar", width=10, anchor="center", padx=5, command=delete_socio).grid(row=0,column=2)
     Button(frame_buttons, text="Actualizar", width=10, anchor="center", padx=5, command=add_data_to_table).grid(row=0,column=3)
+
+def add_or_update_socio(opcion):
+    win = Toplevel()
+    win.configure(background="white")
+    win.geometry("240x130")
+    win.resizable(False, False)
+    win.geometry(
+        "+{}+{}".format(
+            int(win.winfo_screenwidth() / 2 - win.winfo_reqwidth() / 2),
+            int(win.winfo_screenheight() / 2 - win.winfo_reqheight() / 2),
+        )
+    )
+
+    def aceptar(opcion):
+        if opcion == Opciones.ALTA:
+            add_socio(dni_socio.get(), nombre_socio.get(), apellido_socio.get())
+        else:
+            update_socio(socio, dni_socio.get(), nombre_socio.get(), apellido_socio.get())
+        win.destroy()
+        add_data_to_table()
+        root.deiconify()
+
+    def cancelar():
+        win.destroy()
+        root.deiconify()
+
+    dni_socio = IntVar()
+    nombre_socio= StringVar()
+    apellido_socio = StringVar()
+
+    if opcion == Opciones.MODIFICACION:
+        ns = NegocioSocio()
+        socio = ns.buscar(id_socio_seleccionado.get())
+        if socio == None:
+            cancelar()
+        win.title("Modificar socio")
+        dni_socio.set(socio.dni)
+        nombre_socio.set(socio.nombre)
+        apellido_socio.set(socio.apellido)
+    elif opcion == Opciones.ALTA:
+        win.title("Crear socio")        
+
+    Label(win, text="DNI", bg="white").grid(
+        sticky=W + N + S, column=0, row=0, padx=5, pady=5
+    )
+    Entry(win, textvariable=dni_socio, bg="white").grid(
+        sticky=E + N + S, column=1, columnspan=2, row=0, padx=5, pady=5
+    )
+    Label(win, text="Nombre", bg="white").grid(
+        sticky=W + N + S, column=0, row=1, padx=5, pady=5
+    )
+    Entry(win, textvariable=nombre_socio, bg="white").grid(
+        sticky=E + N + S, column=1, columnspan=2, row=1, padx=5, pady=5
+    )
+    Label(win, text="Apellido", bg="white").grid(
+        sticky=W + N + S, column=0, row=2, padx=5, pady=5
+    )
+    Entry(win, textvariable=apellido_socio, bg="white").grid(
+        sticky=E + N + S, column=1, columnspan=2, row=2, padx=5, pady=5
+    )
+    Button(
+        win,
+        text="Aceptar",
+        bg="white",
+        command=lambda: aceptar(opcion),
+        height=1,
+        width=5,
+    ).grid(column=1, row=3, padx=5, pady=5, sticky=W + E + N + S)
+    Button(
+        win,
+        text="Cancelar",
+        bg="white",
+        command=lambda: cancelar(),
+        height=1,
+        width=5,
+    ).grid(column=2, row=3, padx=5, pady=5, sticky=W + E + N + S)
+
+def add_socio(dni, nombre, apellido):
+    socio = Socio(dni=dni, nombre=nombre, apellido=apellido)
+    ns = NegocioSocio()
+    ns.alta(socio)
+
+def update_socio(socio, dni, nombre, apellido):
+    socio.dni = dni
+    socio.nombre = nombre
+    socio.apellido = apellido
+    ns = NegocioSocio()
+    ns.modificacion(socio)
 
 def delete_socio():
        ns = NegocioSocio()
        global id_socio_seleccionado
        ns.baja(id_socio_seleccionado.get())
+       add_data_to_table()
 
 if __name__ == "__main__":
     create_controls()
